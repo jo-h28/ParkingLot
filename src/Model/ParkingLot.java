5552a7ed4ee556f1d7b9c621331e434c013a4736
jo_h28 <joshos1945@yahoo.com>
2019-03-09 14:55:30 +0700

    Refactor ParkingSlot fields

--- src/Model/ParkingLot.java
+++ src/Model/ParkingLot.java
@@ -1,25 +1,25 @@
 package Model;
 
 public class ParkingLot {
-    private Car[] parking_slots;
-    private int available_slots;
+    private Car[] parkingSlots;
+    private int availableSlots;
 
     public ParkingLot(int size){
-        parking_slots = new Car[size];
-        available_slots = size;
+        parkingSlots = new Car[size];
+        availableSlots = size;
         System.out.println("Created a parking lot with " + size + " slots");
     }
 
     public void allocateSlot(String registration_number, String color){
-        if(available_slots == 0){
+        if(availableSlots == 0){
             System.out.println("Sorry, parking lot is full");
             return;
         }
         Car new_car = new Car(registration_number, Color.valueOf(color));
-        for(int i = 0; i < parking_slots.length; ++i){
-            if(parking_slots[i] == null){
-                parking_slots[i] = new_car;
-                available_slots--;
+        for(int i = 0; i < parkingSlots.length; ++i){
+            if(parkingSlots[i] == null){
+                parkingSlots[i] = new_car;
+                availableSlots--;
                 System.out.println("Allocated slot number: " + i + 1);
                 break;
             }
@@ -27,8 +27,9 @@ public class ParkingLot {
     }
 
     public void deallocateSlot(int slot_num){
-        parking_slots[slot_num - 1] = null;
-        available_slots--;
+        parkingSlots[slot_num - 1] = null;
+        availableSlots--;
         System.out.println("Slot number " + 4 + " is free");
     }
+    
 }
