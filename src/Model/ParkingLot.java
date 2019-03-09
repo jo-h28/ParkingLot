4169c5d9f69fff1ae1887ae36f2006e924809e48
jo_h28 <joshos1945@yahoo.com>
2019-03-09 15:13:53 +0700

    Add method structures in ParkingLot class

--- src/Model/ParkingLot.java
+++ src/Model/ParkingLot.java
@@ -10,12 +10,12 @@ public class ParkingLot {
         System.out.println("Created a parking lot with " + size + " slots");
     }
 
-    public void allocateSlot(String registration_number, String color){
+    public void allocateSlot(String registrationNumber, String color){
         if(availableSlots == 0){
             System.out.println("Sorry, parking lot is full");
             return;
         }
-        Car new_car = new Car(registration_number, Color.valueOf(color));
+        Car new_car = new Car(registrationNumber, Color.valueOf(color));
         for(int i = 0; i < parkingSlots.length; ++i){
             if(parkingSlots[i] == null){
                 parkingSlots[i] = new_car;
@@ -26,10 +26,31 @@ public class ParkingLot {
         }
     }
 
-    public void deallocateSlot(int slot_num){
-        parkingSlots[slot_num - 1] = null;
+    public void deallocateSlot(int slotNumber){
+        parkingSlots[slotNumber - 1] = null;
         availableSlots--;
         System.out.println("Slot number " + 4 + " is free");
     }
-    
+
+    public void status(){
+        System.out.println("Slot No.\tRegistration No\tColour");
+        for(int i = 0; i < parkingSlots.length; ++i){
+            if(parkingSlots[i] != null)
+                System.out.println(i + 1
+                        + "\t" + parkingSlots[i].getRegistration_number()
+                        + "\t" + parkingSlots[i].getColor());
+        }
+    }
+
+    public void searchRegistrationNumberByColor(String color){
+        
+    }
+
+    public void searchSlotNumberByRegistrastionNumber(String registrationNumber){
+
+    }
+
+    public void searchSlotNumberByColor(String color){
+
+    }
 }
